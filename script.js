b197a235e9f93dcd6a73aa60902e2c27b1c48137
Christian Yitzak <christian.pelmelay@cakrawala.ac.id>
2025-01-15 09:56:45 +0700

    Add documentation comment

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let calculation = '';
 const output = document.querySelector('.output');
 
 function addCalculation(value) {
+  // Add the number to calculation variable
   calculation += value;
+
+  // Show the number everytime i click it on output.
   output.innerHTML = calculation;
 }
