786247ab61eac6e6ef2bac5154ac9b6fb2d34972
Christian Yitzak <christian.pelmelay@cakrawala.ac.id>
2025-01-15 14:49:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Initialize an empty string to store the current calculation
 let calculation = '';
+
+// Select the DOM element where the calculation result will be displayed
 const output = document.querySelector('.output');
 
+// Function to add a value to the ongoing calculation
 function addCalculation(value) {
-  // Add the number to calculation variable
+  // Append the clicked value (number or operator) to the calculation string
   calculation += value;
 
-  // Show the number everytime i click it on output.
+  // Update the content of the output element to show the current calculation
   output.innerHTML = calculation;
 }
